7c4c57893b9731fe44bf502500732b1d20cdc581
rquinterosv <rafael.quinterosv@gmail.com>
2023-04-08 01:41:24 -0400

    Form-update rooms, missing the +people!

--- src/components/BootForm.js
+++ src/components/BootForm.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap';
+import { Card, Form, Button, Dropdown, InputGroup } from 'react-bootstrap';
+import BookCard from './BookCard';
 
 function BootstrapForm() {
   return (
     <Form>
-
-<Form.Group className='d-flex'>
+      <Form.Group className='d-flex'>
         <div>
          <Form.Label>Nombre</Form.Label>
           <Form.Control type="text" placeholder="First Name"/>
@@ -19,6 +19,29 @@ function BootstrapForm() {
       <Form.Group controlId="formBasicEmail">
         <Form.Label>Correo electrónico</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group controlId="checkIn">
+        <Form.Label>Check-in date</Form.Label>
+        <Form.Control type="date" />
+      </Form.Group>
+
+      <Form.Group controlId="checkOut">
+        <Form.Label>Check-out date</Form.Label>
+        <Form.Control type="date" />
+      </Form.Group>
+
+      <Form.Group controlId="checkOut">
+        <Dropdown>
+          <Dropdown.Toggle variant="secondary" id="dropdownMenuButton">
+            Select a Room
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item>Single Room</Dropdown.Item>
+            <Dropdown.Item>Double Room</Dropdown.Item>
+            <Dropdown.Item>Deluxe Room</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </Form.Group>
 
       <Button variant="primary" type="submit">
